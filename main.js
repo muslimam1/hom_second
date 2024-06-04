1446ba4379abbbb0066e85f4728e62c8ff931355
@muslimam1 <imuslima420@gmail.com>
2024-06-05 03:36:25 +0500

    finish

---
+++ main.js
@@ -0,0 +1,62 @@
+// 1-masala;
+
+// function perimetr (a){
+    // let p = a * 4;
+    // console.log(p);
+// }
+
+// 2- masala;
+
+// function yuza(a){
+    // let s = a*a;
+    // console.log(s);
+// }
+
+// 3- masala;
+
+// function tomonlari(a,b){
+    // let yuza = a * b;
+    // let perimetr = 2 * (a + b);
+    // console.log( yuza, "yuza", perimetr, "perimetr");
+// }
+
+// 4- masala;
+
+// function diametr(d){
+    // let p = 3.14;
+    // let length = p * d;
+    // console.log(length, "length")
+// }
+
+// 5- masala;
+
+// function tomon(a){
+    // let volume = Math.pow(a,3);
+    // let sirti = 6 * Math.pow(a,2);
+    // console.log("volume:", volume, "sirti:", sirti);
+// }
+
+// 6- masala;
+
+// function parallelepiped(a,b,c){
+    // let hajim = a * b *c;
+    // let sirti = 2 * (a*b + b*c + c*a);
+    // console.log(hajim, "hajmi", sirti, "sirti")
+// }
+
+// 7- masala;
+
+// function circle(){
+    // const radius = parseFloat(prompt('radiusni kiriting!'));
+// 
+    // const L = 2 * Math.PI * radius;
+    // const S = Math.PI * radius;
+    // console.log(L, 'uzunligi', S, 'yuzasi')
+// }
+
+// 8- masala;
+
+const a = parseFloat(prompt("a ni kiriting"));
+const b = parseFloat(prompt("b ni kiriting"));
+const mean = (a + b) /2;
+alert(+a+" bilan "+b+" orasidagi ortacha arifmetik  "+ mean, " hisoblanadi")
